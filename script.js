53f130349aeec6503e70db421cdc4617a3199691
adityarajak634-cmyk <adityarajak634@gmail.com>
2026-03-31 10:06:05 +0530

    Add basic arithmetic and discount calculation functions

---
+++ script.js
@@ -0,0 +1,35 @@
+function add() {
+  let a = Number(document.getElementById("num1").value);
+  let b = Number(document.getElementById("num2").value);
+  document.getElementById("result").innerText = "Result: " + (a + b);
+}
+
+function subtract() {
+  let a = Number(document.getElementById("num1").value);
+  let b = Number(document.getElementById("num2").value);
+  document.getElementById("result").innerText = "Result: " + (a - b);
+}
+
+function multiply() {
+  let a = Number(document.getElementById("num1").value);
+  let b = Number(document.getElementById("num2").value);
+  document.getElementById("result").innerText = "Result: " + (a * b);
+}
+
+function divide() {
+  let a = Number(document.getElementById("num1").value);
+  let b = Number(document.getElementById("num2").value);
+  document.getElementById("result").innerText = "Result: " + (a / b);
+}
+
+function calculateDiscount() {
+  let price = Number(document.getElementById("price").value);
+  let discount = Number(document.getElementById("discount").value);
+
+  let final = price - (price * discount / 100);
+
+  document.getElementById("finalPrice").innerText = "Final Price: ₹" + final;
+}
+function toggleDarkMode() {
+  document.body.classList.toggle("dark");
+    }
